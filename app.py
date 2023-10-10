7b631162c85f832634fb0489b5d6db0ac1ce022a
EraaaK <52663832+EraaaK@users.noreply.github.com>
2023-10-10 18:36:17 +0000

    alteração feita

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def second_route():
     return "Teste 2 com rota"
 
+@app.route('/testerota3')
+def second_rooutoute():
+    return "Teste 3 com rota"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
